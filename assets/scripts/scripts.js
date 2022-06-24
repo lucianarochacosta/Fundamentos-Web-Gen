47072f6fab2c2812e2464a339db8630145f722ad
unknown <lucianarocha_14@hotmail.com>
2022-06-23 21:56:58 -0300

    Implementando JS no formulario

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,3 +1,13 @@
-function hello(){
-    alert 
+let nome = document.getElementById('nome')
+let email = document.getElementById('email')
+let assunto = document.getElementById('assunto')
+
+function enviar(){
+    if(nome.value == "" || email.value == "" || assunto.value == ""){
+        alert("Todos os campos são obrigatórios")
+        return false
+    }else{
+        alert(nome.value + ", seu formulário foi enviado")
+        return true
+    }
 }
